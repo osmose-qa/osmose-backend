65baeeaffb555bbbc41cb35150e5722e7cb96188
Famlam <fam.lam@live.nl>
2023-10-29 12:53:48 +0100

    Partial rewrite of deadend oneway detection (#2018)

--- analysers/analyser_osmosis_highway_deadend.py
+++ analysers/analyser_osmosis_highway_deadend.py
@@ -23,18 +23,6 @@
 from modules.OsmoseTranslation import T_
 from .Analyser_Osmosis import Analyser_Osmosis
 
-sql10 = """
--- From https://wiki.postgresql.org/wiki/Array_reverse
--- Inverts the sequence of the elements in an array
-CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS $$
-SELECT ARRAY(
-    SELECT $1[i]
-    FROM generate_subscripts($1,1) AS s(i)
-    ORDER BY i DESC
-);
-$$ LANGUAGE 'sql' STRICT IMMUTABLE;
-"""
-
 sql20 = """
 SELECT
     wid,
