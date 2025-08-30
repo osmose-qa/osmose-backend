0cdfba200869272e504bde67527485f82f362945
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2025-08-30 13:43:08 +0200

    test-polygons: Fix handling of countries with multiple polygon ids
    This aligns with behaviour in modules/Polygon.py, by:
      - iterating poly_id on index.py to generate each relation
      - calling get_poly.py with the full poly_id list

--- tools/test-polygons.py
+++ tools/test-polygons.py
@@ -18,32 +18,46 @@ for c in osmose_config.config.values():
 
     print('  ', c.country, c.polygon_id)
 
+    # polygon_id can be an integer, or a list of integers
+    polygon_id = c.polygon_id
+    if isinstance(polygon_id, int):
+        polygon_id = (polygon_id, )
+
     # generate relation boundary
-    try:
-        r = requests.post(relation_generation_url, params={'id': c.polygon_id}, data={'refresh': 1}, timeout=120)
-    except requests.exceptions.Timeout:
-        print("      Timeout")
-        fails.append([c.country, c.polygon_id, 'Timeout'])
-        continue
-    if r.status_code == 500:
-        print("      Geom Error -", r.url)
-        fails.append([c.country, c.polygon_id, 'Geom Error'])
-        continue
-    elif r.status_code != 200:
-        print("      Error -", r.url)
-        fails.append([c.country, c.polygon_id, 'Error'])
+    relation_failing = False
+    for poly_id in polygon_id:
+        try:
+            r = requests.post(relation_generation_url, params={'id': str(poly_id)}, data={'refresh': 1}, timeout=120)
+        except requests.exceptions.Timeout:
+            print("      Timeout")
+            fails.append([c.country, polygon_id, 'Timeout'])
+            relation_failing = True
+            continue
+        if r.status_code == 500:
+            print("      Geom Error -", r.url)
+            fails.append([c.country, polygon_id, 'Geom Error'])
+            relation_failing = True
+            continue
+        elif r.status_code != 200:
+            print("      Error -", r.url)
+            fails.append([c.country, polygon_id, 'Error'])
+            relation_failing = True
+            continue
+
+    if relation_failing:
         continue
 
     # get associated poly file
     try:
-        r = requests.get(polygon_union_url, params={'id': c.polygon_id, 'params': 0}, timeout=120)
+        poly_id = ",".join(map(str, polygon_id))
+        r = requests.get(polygon_union_url, params={'id': poly_id, 'params': '0'}, timeout=120)
     except requests.exceptions.Timeout:
         print("      Poly Timeout")
-        fails.append([c.country, c.polygon_id, 'Poly Timeout'])
+        fails.append([c.country, polygon_id, 'Poly Timeout'])
         continue
     if r.status_code != 200:
         print("      Bad geom -", r.url)
-        fails.append([c.country, c.polygon_id, 'Bad geom'])
+        fails.append([c.country, polygon_id, 'Bad geom'])
 
 if len(fails) > 0:
     print(fails)
