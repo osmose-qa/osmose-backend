3be075bf3e60fdd0c79d797aa2db72bdd4f92652
Simon <94762919+simonpct@users.noreply.github.com>
2025-10-08 23:40:56 +0200

    Fix duplicate key in mapping1 and correct mapping2 syntax

--- analysers/analyser_merge_public_transport_FR_stan.py
+++ analysers/analyser_merge_public_transport_FR_stan.py
@@ -56,9 +56,8 @@ class Analyser_Merge_Public_Transport_FR_stan(Analyser_Merge_Point):
                         "gtfs:stop_code:FR-GES-STAN": "stop_code",
                         "gtfs:stop_id:FR-GES-STAN": "stop_id",
                         "gtfs:stop_name:FR-GES-STAN": "stop_name",
-                        "gtfs:stop_id:FR-GES-STAN": "stop_code",
                         "wheelchair": lambda fields: self.wheelchair_boarding[fields.get("wheelchair_boarding")]},
-                    mapping2 = {"name": "stop_name", ref: "stop_id"},
+                    mapping2 = {"name": "stop_name", "ref": "stop_id"},
                     text = lambda tags, fields: T_("{0} stop of {1}", place, fields["stop_name"]) )))
 
     wheelchair_boarding = {
