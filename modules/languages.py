fee81c117eb26d8c8a33c303d7921db065f31bba
Kyℓe Hensel <k-yle@users.noreply.github.com>
2026-04-22 22:41:19 +1000

    add missing script definition for language mi

--- modules/languages.py
+++ modules/languages.py
@@ -73,6 +73,7 @@ language2scripts = {
     'lt': ['Latin'],
     'lv': ['Latin'],
     'mg': ['Latin'],
+    'mi': [u'[A-Za-zĀĒĪŌŪāēīōū]'],
     'mn': ['Cyrillic'],
     'ms': ['Latin'],
     'my': None, # Birman
