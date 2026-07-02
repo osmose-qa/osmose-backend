9bc72aad81cf12ff757d82e26d20562a9bcb3475
Kendell <KTibow@users.noreply.github.com>
2026-07-02 09:35:47 -0700

    Detect incorrect `crossing:island=yes` (#2760)
    * Add highway traffic island analyser
    
    * Use distinct traffic island Osmose class
    
    * Classify split crossing island tag as incorrect tag
    
    * Tune traffic island analyser query
    
    * Apply suggestions from @frodrigo
    
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>
    
    * Trigger CI to reproduce failures
    
    * Fix 3 test failures for highway-traffic-island-analyser
    
    - Add touched_ways and not_touched_ways table handlers in
      requires_tables_build (Analyser_Osmosis.py)
    - Add missing version='1' attributes to OSM test file elements
      (Osmosis 0.48 requires version attr for OSM 0.6)
    
    The KeyError: 'class' was a cascading symptom: test_change_full
    crashed on touched_ways, producing partial XML without <class>,
    which then failed in test_cmp_normal_change comparison.
    
    * Fix: add missing timestamp attributes to OSM test file elements
    
    Osmosis 0.48 requires both version and timestamp attributes for OSM 0.6
    elements.
    
    * Fix: create touched_ways/not_touched_ways views inline instead of via requires_tables_build
    
    The reviewer noted that touched_ways and not_touched_ways should not be
    declared in requires_tables_diff nor handled by the base class's
    requires_tables_build. Instead, create these temporary views inline in
    analyser_osmosis_diff() using self.create_view_touched() and
    self.create_view_not_touched(), matching the pattern used by other
    analysers (e.g. analyser_osmosis_highway_vs_building.py).
    
    * Remove redundant requires_tables_full/diff = ['ways']
    
    The 'ways' table is a base OSM table that is always present in the
    database schema (created by Osmosis during import). Specifying it in
    requires_tables_full or requires_tables_diff is unnecessary — ways is
    implicit. No other analyser declares 'ways' as a required table.
    
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>
    
    * Address review feedback: cleanup touched views, add order-by tiebreaker, valid test tagging
    
    - Remove redundant create_view_touched/create_view_not_touched calls
      (touched_ways and not_touched_ways already exist from CreateTouched.sql)
    - Add traffic_island.id to ORDER BY for deterministic DISTINCT ON
    - Add highway=footway to all traffic island test ways for realistic tagging
    
    Co-authored-by: Famlam <famlam@users.noreply.github.com>
    
    * use class 1
    
    * switch to highways table
    
    * assume highway=footway
    
    ---------
    
    Co-authored-by: ktibow-qa[bot] <ktibow-qa[bot]@users.noreply.github.com>
    Co-authored-by: Frédéric Rodrigo <fred.rodrigo@gmail.com>
    Co-authored-by: Famlam <famlam@users.noreply.github.com>

---
+++ analysers/analyser_osmosis_highway_traffic_island.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+###########################################################################
+##                                                                       ##
+## Copyright Osmose Contributors 2026                                    ##
+##                                                                       ##
+## This program is free software: you can redistribute it and/or modify  ##
+## it under the terms of the GNU General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or     ##
+## (at your option) any later version.                                   ##
+##                                                                       ##
+## This program is distributed in the hope that it will be useful,       ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+## GNU General Public License for more details.                          ##
+##                                                                       ##
+## You should have received a copy of the GNU General Public License     ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>. ##
+##                                                                       ##
+###########################################################################
+
+from modules.OsmoseTranslation import T_
+from .Analyser_Osmosis import Analyser_Osmosis
+
+
+sql10 = """
+SELECT DISTINCT ON (crossing.id)
+    crossing.id,
+    traffic_island.id,
+    ST_AsText(way_locate(crossing.linestring))
+FROM
+    {0}highways AS crossing
+    JOIN {1}highways AS traffic_island ON
+        traffic_island.linestring && crossing.linestring AND
+        traffic_island.id != crossing.id AND
+        traffic_island.nodes && crossing.nodes AND
+        traffic_island.tags != ''::hstore AND
+        traffic_island.tags?'footway' AND
+        traffic_island.tags->'footway' = 'traffic_island'
+WHERE
+    crossing.tags != ''::hstore AND
+    crossing.tags?'crossing:island' AND
+    crossing.tags->'crossing:island' = 'yes' AND
+    crossing.tags?|ARRAY['footway', 'cycleway', 'path'] AND
+    (
+        crossing.tags->'footway' = 'crossing' OR
+        crossing.tags->'cycleway' = 'crossing' OR
+        crossing.tags->'path' = 'crossing'
+    )
+ORDER BY
+    crossing.id,
+    traffic_island.id
+"""
+
+
+class Analyser_Osmosis_Highway_Traffic_Island(Analyser_Osmosis):
+
+    requires_tables_common = ['highways']
+    requires_tables_diff = ['touched_highways', 'not_touched_highways']
+
+    def __init__(self, config, logger = None):
+        Analyser_Osmosis.__init__(self, config, logger)
+        self.classs_change[1] = self.def_class(item = 3040, level = 3, tags = ['tag', 'highway', 'footway', 'fix:survey'],
+            title = T_('Crossing island tag on a split crossing'),
+            detail = T_(
+'''A crossing way is tagged with `crossing:island=yes` and connects to a
+separately mapped `footway=traffic_island`.
+
+When a traffic island is mapped as its own way, the crossing ways leading
+to it usually should not also be tagged as having a crossing island.'''),
+            fix = T_(
+'''Check the highlighted crossing way. If it is a segment leading to
+the separately mapped traffic island, remove `crossing:island=yes`. You may add
+`crossing:island=no` for clarity.
+
+If the crossing way continues all the way across the street and over the island
+as one continuous crossing, keep `crossing:island=yes`.'''),
+            trap = T_(
+'''If you are not sure whether the crossing way ends at the island or continues
+over it, do not change the tag.'''))
+
+        self.callback10 = lambda res: {"class": 1, "data": [self.way_full, self.way_full, self.positionAsText]}
+
+    def analyser_osmosis_full(self):
+        self.run(sql10.format("", ""), self.callback10)
+
+    def analyser_osmosis_diff(self):
+        # Match issues when either the crossing way or the traffic island way is touched.
+        # touched_highways and not_touched_highways views are created by requires_tables_build.
+        self.run(sql10.format("touched_", ""), self.callback10)
+        self.run(sql10.format("not_touched_", "touched_"), self.callback10)
+
+
+###########################################################################
+
+from .Analyser_Osmosis import TestAnalyserOsmosis
+
+
+class Test(TestAnalyserOsmosis):
+    @classmethod
+    def setup_class(cls):
+        from modules import config
+        TestAnalyserOsmosis.setup_class()
+        cls.analyser_conf = cls.load_osm("tests/osmosis_highway_traffic_island.osm",
+                                         config.dir_tmp + "/tests/osmosis_highway_traffic_island.test.xml",
+                                         {"proj": 2154})
+
+    def test_classes(self):
+        with Analyser_Osmosis_Highway_Traffic_Island(self.analyser_conf, self.logger) as a:
+            a.analyser()
+
+        self.root_err = self.load_errors()
+        self.check_err(cl="1", elems=[("way", "100"), ("way", "200")])
+        self.check_err(cl="1", elems=[("way", "110"), ("way", "210")])
+        self.check_err(cl="1", elems=[("way", "120"), ("way", "220")])
+        self.check_num_err(3)
